22f25cd84d4dffe05fd6425382c5906d0e59870d
jakubprogramming <jakub94@gmx.de>
2023-03-08 11:48:34 +0100

    add *.env to gitignore

---
+++ qsfs/myfirstproject/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    grid = {
+      source = "threefoldtech/grid"
+    }
+  }
+}
+
+provider "grid" {
+    mnemonics = "FROM THE CREATE TWIN STEP"
+    network = "dev" # or test to use testnet
+}
